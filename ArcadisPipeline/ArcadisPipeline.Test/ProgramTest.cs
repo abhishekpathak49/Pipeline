399a796275e48d9118e7d07fcfcc64ee263e72e1
Abhishek pathak <abhishekpathak49@gmail.com>
2022-01-18 15:28:16 +0530

    Added the test project

---
+++ ArcadisPipeline/ArcadisPipeline.Test/ProgramTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArcadisPipeline;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcadisPipeline.Tests
+{
+    [TestClass()]
+    public class ProgramTest
+    {
+        [TestMethod()]
+        public void IsAdultAgeTest()
+        {
+            Assert.IsTrue(new Program().IsAdultAge(19));
+        }
+    }
+}
---
+++ ArcadisPipeline/ArcadisPipeline/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArcadisPipeline
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsAdultAge(int age)
+        {
+            return age > 17 ? true : false; 
+        }
+    }
+}
